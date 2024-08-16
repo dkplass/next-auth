4ef1f668e97efadb8072575df6cb4fdaae19723b
Hans Hou <dkplass@gmail.com>
2024-08-16 14:36:54 +0800

    implement login form

--- components/auth/LoginForm.tsx
+++ components/auth/LoginForm.tsx
@@ -1,6 +1,56 @@
+"use client";
+
+import * as z from "zod";
+import { useState, useTransition } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { LoginSchema } from "@/schemas";
+import { login } from "@/actions/login";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "../ui/button";
 import CardWrapper from "./CardWrapper";
+import FormError from "../FormError";
+import FormSuccess from "../FormSuccess";
 
 const LoginForm = () => {
+  const [success, setSuccess] = useState<string | undefined>("");
+  const [error, setError] = useState<string | undefined>("");
+  const [isPending, startTransition] = useTransition();
+
+  const form = useForm<z.infer<typeof LoginSchema>>({
+    resolver: zodResolver(LoginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof LoginSchema>) => {
+    setSuccess("");
+    setError("");
+
+    startTransition(() => {
+      // server action
+      login(values).then((data) => {
+        setSuccess(data.success);
+        setError(data.error);
+      });
+
+      // not server action
+      // axios.post("api", values)
+    });
+  };
+
   return (
     <CardWrapper
       headerLabel="Welcome back"
@@ -8,7 +58,54 @@ const LoginForm = () => {
       backButtonHref="/auth/register"
       showSocial
     >
-      login-form
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+          <div className="space-y-4">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      disabled={isPending}
+                      placeholder="hans@example.com"
+                      type="text"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      disabled={isPending}
+                      placeholder="******"
+                      type="password"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <FormError message={error} />
+          <FormSuccess message={success} />
+          <Button disabled={isPending} type="submit" className="w-full">
+            Confirm
+          </Button>
+        </form>
+      </Form>
     </CardWrapper>
   );
 };
---
+++ schemas/index.ts
@@ -0,0 +1,7 @@
+import * as z from "zod";
+
+export const LoginSchema = z.object({
+  email: z.string().email("Email is required"),
+  password: z.string().min(1, "Password is required"),
+  code: z.optional(z.string()),
+});
